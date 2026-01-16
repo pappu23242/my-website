bd835ab718e56be1876fd5f8e90e118db6beeb2b
Pappu Thakur <39671089+pappu23242@users.noreply.github.com>
2026-01-16 18:11:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example JS: smooth scroll
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href'))?.scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
